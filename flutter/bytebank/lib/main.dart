188f1409751088b7aadb3b2a2eb903dfb1f89514
Artur Oscar <artur.junior@ufba.br>
2020-01-25 17:08:49 -0300

    Fim da aula 01.

--- flutter/bytebank/lib/main.dart
+++ flutter/bytebank/lib/main.dart
@@ -1,3 +1,26 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(Text("\n\n\n\n\nBem vindo ao Bytebank", textDirection: TextDirection.rtl, textAlign: TextAlign.center));
+void main() => runApp(Column(
+      children: <Widget>[
+        Text(
+          'Deliver features faster',
+          textDirection: TextDirection.rtl,
+        ),
+        Text(
+          'Craft beautiful UIs',
+          textDirection: TextDirection.ltr,
+        ),
+        Text(
+          'Craft beautiful UIs',
+          textDirection: TextDirection.ltr,
+        ),
+        Column(
+          children: <Widget>[
+            Text(
+              'Filho do coluna 2',
+              textDirection: TextDirection.ltr,
+            )
+          ],
+        )
+      ],
+    ));
