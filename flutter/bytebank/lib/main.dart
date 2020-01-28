dffd27a72267eacf8920e899532e81639e252b10
Artur Oscar <artur.junior@ufba.br>
2020-01-27 22:50:57 -0300

    Aula 2 - Implementando design Item.

--- flutter/bytebank/lib/main.dart
+++ flutter/bytebank/lib/main.dart
@@ -3,6 +3,23 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
+          body: Column(
+            children: <Widget>[
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.monetization_on),
+                  title: Text("100.0"),
+                  subtitle: Text("0001"),
+                ),
+              ),Card(
+                child: ListTile(
+                  leading: Icon(Icons.monetization_on),
+                  title: Text("200.0"),
+                  subtitle: Text("0001"),
+                ),
+              ),
+            ],
+          ),
           appBar: AppBar(
             title: const Text("Transferências"),
             backgroundColor: Color.fromRGBO(0, 70, 10, 1),
