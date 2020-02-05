79bc25368ece7a3467b65d068a335accc6529b58
Artur Oscar <artur.junior@ufba.br>
2020-02-04 22:45:09 -0300

    Terminando aula 3 - Utilizando SnackBar e fazendo transferência.

--- flutter/bytebank/lib/main.dart
+++ flutter/bytebank/lib/main.dart
@@ -8,20 +8,66 @@ class ByteBankApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: FormularioTransferencia(),
-
       ),
     );
   }
 }
 
 class FormularioTransferencia extends StatelessWidget {
+  final TextEditingController _controladorCampoNumeroConta =
+  TextEditingController();
+  final TextEditingController _controladorCampoValor = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Criando Transferência"),
       ),
-      body: Text("Teste")
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _controladorCampoNumeroConta,
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              decoration: InputDecoration(
+                  labelText: "Número da conta", hintText: "0000"),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _controladorCampoValor,
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              decoration: InputDecoration(
+                  icon: Icon(Icons.monetization_on),
+                  labelText: "Valor",
+                  hintText: "0.00"),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          RaisedButton(
+            child: Text('Confirmar'),
+            onPressed: () {
+              int _numeroConta = int.tryParse(
+                  _controladorCampoNumeroConta.text);
+              double _valor = double.tryParse(_controladorCampoValor.text);
+              if (_numeroConta != null && _valor != null) {
+                final transferenciaCriada = Transferencia(_valor, _numeroConta);
+                debugPrint("$transferenciaCriada");
+                Scaffold.of(context).showSnackBar(SnackBar(content: Text("$transferenciaCriada"),));
+              } else
+                debugPrint("Null...");
+            },
+          ),
+        ],
+      ),
     );
   }
 }
@@ -41,15 +87,14 @@ class ListaTransferencia extends StatelessWidget {
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add), onPressed: () {},
+        child: Icon(Icons.add),
+        onPressed: () {},
       ),
     );
   }
-
 }
 
 class ItemTransferencia extends StatelessWidget {
-
   final Transferencia _transferencia;
 
   ItemTransferencia(this._transferencia);
@@ -64,9 +109,15 @@ class ItemTransferencia extends StatelessWidget {
     );
   }
 }
-class Transferencia{
+
+class Transferencia {
   final double _valor;
   final int _numeroConta;
 
   Transferencia(this._valor, this._numeroConta);
+
+  @override
+  String toString() {
+    return 'Transferencia{_valor: $_valor, _numeroConta: $_numeroConta}';
+  }
 }
