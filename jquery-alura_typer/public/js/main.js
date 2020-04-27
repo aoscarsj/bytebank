4408fb4000a74b878edfa1aaa791936e09d36fd7
Artur Oscar <artur.junior@ufba.br>
2020-04-27 11:14:16 -0300

    Utilizando textarea.
    Fim da aula 2.

--- jquery-alura_typer/public/js/main.js
+++ jquery-alura_typer/public/js/main.js
@@ -2,8 +2,20 @@
 
 var frase = $(".frase").text();
 var numPalavras = frase.split(" ").length;
-
 var tamanhoFrase = $("#tamanho-frase");
 tamanhoFrase.text(numPalavras);
 
-console.log(frase);
+var campo = $(".campo-digitacao");
+
+campo.on("input", function () {
+   var conteudo = campo.val();
+   var qtdPalavras = conteudo.split(/\S+/).length; //expressao regular que busca por qualquer espaco vazio
+   var qtdLetras = conteudo.length;
+
+   $("#contador-palavras").text(qtdPalavras-1);
+   $("#contador-caracteres").text(qtdLetras);
+});
+
+function f () {
+
+}
