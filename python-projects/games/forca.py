b2e9057784a6527236ae2224a31ece2e472be8e1
Artur Oscar <artur.junior@ufba.br>
2020-05-19 09:53:32 -0300

    Curso dois - dev o forca.py

--- python-projects/games/forca.py
+++ python-projects/games/forca.py
@@ -1,11 +1,28 @@
-
 def play():
     print("******************************************")
     print("********Bem vindo ao jogo da Forca*******!")
     print("******************************************")
 
+    palavra_secreta = "banana"
+    palavra_secreta = palavra_secreta.upper()
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        chute = input("Qual a letra? ")
+        chute = chute.strip().upper()
+        print(chute)
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print("Encontrei a letra {} na posição {}".format(letra,index))
+            index += 1
+
 
     print("Fim do jogo")
 
-if(__name__ == "__main__"):
+
+if (__name__ == "__main__"):
     play()
